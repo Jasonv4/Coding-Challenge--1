0ad04355e4754fa8f410dc04e128870829709422
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-01-26 22:42:10 -0500

    Task 2-Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,11 @@ var isActive = true;
 console.log(typeof employeeName)
 console.log(typeof employeeID)
 console.log(typeof isActive)
+
+//Task 2: Primative Data Types
+let productName = "Cheetos";
+const productPrice = 2.99;
+var isavailable = true;
+console.log(typeof productName)
+console.log(typeof productPrice)
+console.log(typeof isAvailable)
