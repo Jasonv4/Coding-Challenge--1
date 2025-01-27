6a38c32981c6c5b32ccfc62c7a48f3d6401e9639
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-01-26 22:52:44 -0500

    Task 3- Financial Transactions

--- cc_1.js
+++ cc_1.js
@@ -9,7 +9,19 @@ console.log(typeof isActive)
 //Task 2: Primative Data Types
 let productName = "Cheetos";
 const productPrice = 2.99;
-var isavailable = true;
+var isAvailable = true;
 console.log(typeof productName)
 console.log(typeof productPrice)
 console.log(typeof isAvailable)
+
+//Task 3 Number Data Type
+let accountBalance = 300
+console.log(accountBalance)
+accountBalance += 100
+console.log(accountBalance)
+accountBalance -= 2.99 
+console.log(accountBalance)
+accountBalance /= 2
+console.log(accountBalance)
+accountBalance *3 
+console.log(accountBalance) 
