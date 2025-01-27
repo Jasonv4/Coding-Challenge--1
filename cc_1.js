80a0ecb4e06ed44aa74731137c1f3cc20eba9429
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-01-26 22:31:40 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+//Task 1: Variable
+let employeeName = "Juan Carlos";
+const employeeID = "0024";
+var isActive = true;
+console.log(typeof employeeName)
+console.log(typeof employeeID)
+console.log(typeof isActive)
